a89b233f4831d546bd280d872927ffa181931e9e
probitsdevelopment <probitsdevelopment@gmail.com>
2026-02-07 15:08:51 +0530

    Change server port from 3001 to 3000

--- Timesheetapp/backend/src/server.js
+++ Timesheetapp/backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Import middleware
 const { corsConfig, securityHeaders } = require('./middleware/securityMiddleware');
