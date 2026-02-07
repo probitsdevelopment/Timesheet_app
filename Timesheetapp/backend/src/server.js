d65ffca7281818602bf48b5ad64d6b33c5c32bf7
probitsdevelopment <probitsdevelopment@gmail.com>
2026-02-07 16:20:27 +0530

    Remove default port fallback in server.js

--- Timesheetapp/backend/src/server.js
+++ Timesheetapp/backend/src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT ||  8080;
+const PORT = process.env.PORT;
 
 // Import middleware
 const { corsConfig, securityHeaders } = require('./middleware/securityMiddleware');
