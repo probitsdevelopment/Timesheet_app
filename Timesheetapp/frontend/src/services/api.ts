7f124661afe0e212aaaa86c6097a74c088c8bada
probitsdevelopment <probitsdevelopment@gmail.com>
2026-02-08 20:08:54 +0530

    uncommented

--- Timesheetapp/frontend/src/services/api.ts
+++ Timesheetapp/frontend/src/services/api.ts
@@ -3,8 +3,8 @@
 
 import { baseURL } from "./networkConstant";
 
-// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/';
-const API_BASE_URL = baseURL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/';
+// const API_BASE_URL = baseURL;
 const normalizeEndpoint = (endpoint: string) => {
   if (endpoint.startsWith('/')) return endpoint;
   return '/' + endpoint;
